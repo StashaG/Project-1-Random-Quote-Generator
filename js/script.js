4c1a78799c585ed58350ca5df0dbbfac69e04a85
Stacey Graham <stacey.graham1106@gmail.com>
2019-08-12 21:56:57 -0400

    Created quote array of objects

--- js/script.js
+++ js/script.js
@@ -15,18 +15,49 @@ project 1 - A Random Quote Generator
   Use console.log() to log your array of quotes to the console.
 ***/
 
-var quotes = {
-  qoute: "But success is not the absence of failure, it's the persistence through failure. ~ Mattie James, 2019",
-  source: "Mattie James",
-  citation: "Mattie Minute Newsletter",
-  year: "2019",  
-};
+var quotes = [
+  { 
+    qoute: "But success is not the absence of failure, it's the persistence through failure.",
+    source: "Mattie James",
+    citation: "Mattie Minute Newsletter",
+    year: "2019",  
+},
+{
+    quote: "If you are persistent, you will get it. If you are consistent, you will keep it.",
+    source: "Mattie James",
+    citation: "Mattie Minute Newsletter",
+    year: "2019",
+},
+{
+  quote: "All businesses are about relationships and the tech industry is no different ~ , 2019",
+  source: "Ruben Harris",
+  citation: "Medium",
+  year: "2016", 
+},
+{
+  quote: "I promise you somebody took that same situation you're in right now and bodied it. Find a solution.",
+  source: "Ruben Harris",
+  citation: "Twitter",
+  year: "2019", 
+},
+{
+  quote: "Coding is easier when you do it with a community of people who are just as passionate about coding as you",
+  source: "Saron Yitbarek",
+  citation: "Founder of CodeNewbie",
+},
+{
+  quote: "Coding is easier when you do it with a community of people who are just as passionate about coding as you",
+  source: "Saron Yitbarek",
+  citation: "Founder of CodeNewbie",
+},
+{
+  quote: "The best jobs aren't publicly listed. You have to dig for them, invent them, " 
+  + "or convince someone it's worth creating just for you.",
+  source: "Sahil Lavingia",
+  citation: "Twitter"
+  year: "2019",
+},
 
-/*"If you are persistent, you will get it. If you are consistent, you will keep it. ~ Mattie James, 2019",
-"All businesses are about relationships and the tech industry is no different ~ Ruben Harris, 2019",
-"Coding is easier when you do it with a community of people who are just as passionate about coding as you ~ Saron Yitbarek, Founder of CodeNewbie",
-"The best jobs aren't publicly listed. You have to dig for them, invent them, or convince someone it's worth creating just for you. ~ Sahil Lavingia, Twitter, 2019"
-*/
 
 /***
   Create the `getRandomQuote` function to:
